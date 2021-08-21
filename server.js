45b6d0d8229ae551aaad67b66f20e5a7e16821b1
timo <timochen77@gmail.com>
2021-08-21 02:40:13 +0000

    worked in remote server

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "/public", "index.html"));
+});
+
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
